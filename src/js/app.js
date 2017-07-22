19b7fe7400ce61fa938c35818f8f42814c108eaa
Boring <boring@Boring-Book.local>
2017-07-22 15:47:16 +0800

    Just keep dist

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import sentences from '../data/sentences.json';
-
-const sentence = sentences[Math.floor(Math.random() * sentences.length)];
-[
-	'en',
-	'zh',
-].forEach((lang) => {
-	document.getElementById(lang).innerHTML = sentence[lang];
-});
