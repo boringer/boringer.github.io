faf4a9be2b91b77aa97e96952779f8954d3c1378
Boring <boring@Boring-Book.local>
2017-07-31 23:35:08 +0800

    Remove Chinese

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,4 @@ import '../css/index.css';
 import sentences from '../data/sentences.json';
 
 const sentence = sentences[Math.floor(Math.random() * sentences.length)];
-[
-	'en',
-	'zh',
-].forEach((lang) => {
-	document.getElementById(lang).innerHTML = sentence[lang];
-});
+document.getElementById('sentence').innerHTML = sentence;
